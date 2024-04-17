55c745bad44efc7e2972afce2df8d6bc8505a31a
HUMBEF0OL <123amitrana0123@gmail.com>
2024-04-17 18:20:47 +0530

    theme setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
+import { Typography } from "@mui/material";
 import styles from "./page.module.css";
+import { Oxygen } from 'next/font/google'
+// { 
+//   display: 'swap',
+//   variable: '--font-oxygen',
+//   subsets: ['latin'],
+// }
+const oxygen = Oxygen({
+  weight: ["300"],
+  subsets: ['latin']
+})
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className={styles.container}>
       <div className={styles.description}>
-
+        <Typography sx={{
+          fontFamily: 'Fira Code'
+        }}>dummy in cascadia code</Typography>
       </div>
     </main>
   );
