2af8597f028aeac14da73abcf2e7e3be6e5cc05f
HUMBEF0OL <123amitrana0123@gmail.com>
2024-04-22 19:20:00 +0530

    started header design

--- src/components/Common/Header.tsx
+++ src/components/Common/Header.tsx
@@ -1,17 +1,46 @@
-import { Box, Typography } from '@mui/material'
-import React from 'react'
+"use client"
+import { AppBar, Box, Container, MenuItem, Toolbar, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+
+const routes = ['home', 'about', 'resume', 'portfolio'] as const;
+type Routes = typeof routes[number];
 
 const Header = () => {
+    const [currentRoute, setCurrentRoute] = useState<Routes>('home')
+
+    useEffect(() => {
+        console.log(currentRoute);
+    }, [currentRoute])
+    const handleMenuSelection = (newPage: Routes) => {
+        setCurrentRoute(newPage)
+    };
     return (
-        <Box
-            sx={{
-                height: '100%'
-            }}
+        <AppBar position="fixed" sx={{
+            left: 0,
+            top: 0,
+            maxWidth: '550px',
+            display: { md: 'block', xs: 'none', sm: 'none' },
+            backdropFilter: 'blur(10px)',
+            backgroundColor: 'background.secondary',
+            mx: 'auto'
+        }}
         >
-            <Typography variant="h4" align="center" color={'text.secondary'}>
-                Header
-            </Typography>
-        </Box>
+            <Container sx={{ width: '100%' }}>
+                <Toolbar disableGutters>
+                    <Box sx={{ flexGrow: 1, flexDirection: 'row', justifyContent: 'space-between', display: 'flex' }}>
+                        {routes.map((page) => (
+                            <MenuItem
+                                key={page}
+                                onClick={() => { handleMenuSelection(page) }}
+                            >
+                                <Typography textAlign="center">{page.toUpperCase()}</Typography>
+                            </MenuItem>
+                        ))}
+                    </Box>
+
+                </Toolbar>
+            </Container>
+        </AppBar>
     )
 }
 
