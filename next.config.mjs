595abf5672cf1ddc67fb874e949041ad61e416d0
HUMBLEFOOL <123amitrana0123@gmail.com>
2025-06-30 19:36:55 +0530

    chore: reset

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['media.dev.to', 'avatars.githubusercontent.com', 'github-readme-stats.vercel.app', 'github-readme-activity-graph.vercel.app', 'github-readme-streak-stats.herokuapp.com', 'stardev.io', 'github-profile-summary-cards.vercel.app', 'github-profile-trophy.vercel.app'],
-        dangerouslyAllowSVG: true,
-    }
-};
-
-export default nextConfig;
